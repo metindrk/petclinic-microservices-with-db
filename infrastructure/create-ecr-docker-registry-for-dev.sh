ee543385c3d6e8cfdae99a2d58b6ab8184bccd3e
metindrk <mtndrg@gmail.com>
2022-07-05 17:21:02 +0000

    added rancher setup files

--- infrastructure/create-ecr-docker-registry-for-dev.sh
+++ infrastructure/create-ecr-docker-registry-for-dev.sh
@@ -1,9 +1,5 @@
 PATH="$PATH:/usr/local/bin"
-<<<<<<< HEAD
-APP_REPO_NAME="clarusway-repo/petclinic-app-dev"
-=======
 APP_REPO_NAME="mtndrk/petclinic-app-dev"
->>>>>>> feature/msp-11
 AWS_REGION="us-east-1"
 
 aws ecr create-repository \
